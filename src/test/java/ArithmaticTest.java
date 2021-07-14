d447c9699bca384eadb3a0c99b8bf9a5bc05b8f4
PensiveDragon <matthew_bfg2k@hotmail.com>
2021-07-14 20:57:17 +0100

    Updated to JUnit5

--- src/test/java/ArithmaticTest.java
+++ src/test/java/ArithmaticTest.java
@@ -1,20 +1,20 @@
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Test;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
-import static org.hamcrest.CoreMatchers.is;
-import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
 
 public class ArithmaticTest {
 
     private final Arithmatic subject = new Arithmatic();
 
-    @Before
+    @BeforeEach
     public void setUp() {
         System.out.println("I am in the setup method.");
     }
 
-    @After
+    @AfterEach
     public void tearDown() {
         System.out.println("I am in the tear down method.");
     }
@@ -22,13 +22,13 @@ public class ArithmaticTest {
     @Test
     public void arithmeticFivePlusTenEqualsFifteen() {
         System.out.println("Testing 5+10=15");
-        assertThat(subject.add(5,10), is(15));
+        assertThat(subject.add(5,10), equalTo(15));
     }
 
     @Test
     public void arithmeticZeroPlusSixEqualsSix() {
         System.out.println("Testing 0+6=6");
-        assertThat(subject.add(0,6), is(6));
+        assertThat(subject.add(0,6), equalTo(6));
     }
 
 
--- src/test/java/RomanNumeralsTest.java
+++ src/test/java/RomanNumeralsTest.java
@@ -1,7 +1,7 @@
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
 import static org.hamcrest.CoreMatchers.is;
-import static org.junit.Assert.*;
+import static org.hamcrest.MatcherAssert.assertThat;
 
 public class RomanNumeralsTest {
 
